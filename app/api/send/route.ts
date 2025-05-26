368c58b227c8b5b8d5d9dd33c247c11a794a6e01
kirtan0101 <kjpatel200022@gmail.com>
2025-05-26 14:17:15 +0530

    changed url to hard coded

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -11,32 +11,26 @@ export async function POST(req: Request) {
     try {
         const { email } = await req.json();
         const res = await fetchUserNameByEmail(email);
-        if (res.status == 404) {
+        if(res.status == 404) {
             return Response.json({ error: res.message }, { status: res.status })
         }
-        let name = '';
-
-        if (res.status === 200) {
+        let name='';
+        
+        if(res.status === 200) {
             name = res.data?.name;
         } else {
             return Response.json({ error: res.message }, { status: res.status })
         }
         const token = sign({ email }, process.env.JWT_SECRET!, { expiresIn: "15m" })
-        const baseUrl =
-            process.env.NODE_ENV === 'development'
-                ? 'http://localhost:3000'
-                : 'https://next-auth-five-ruby.vercel.app';
-
-        const resetUrl = `${baseUrl}/reset?token=${token}`;
-
+        const resetUrl = `https://next-auth-five-ruby.vercel.app/reset?token=${token}`;
 
         const { data, error } = await resend.emails.send({
             from: 'Kiton <onboarding@resend.dev>',
             to: [email],
             subject: 'Reset your kiton account password',
-            react: EmailTemplate({ firstName: name }, { resetUrl }),
+            react: EmailTemplate({ firstName: name }, {resetUrl}),
         });
-
+        
         if (error) {
             console.log(error);
             return Response.json({ error }, { status: 500 });
