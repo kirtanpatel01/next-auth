cfcd288ac94c979c0afc43a4e6532a30593e3953
kirtan0101 <kjpatel200022@gmail.com>
2025-05-26 14:02:12 +0530

    fix: update user name retrieval in Home component for better accuracy

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import { auth } from "@/auth";
 import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
-import { logoutAction } from "@/lib/auth.actions";
+import { fetchUserNameByEmail, logoutAction } from "@/lib/auth.actions";
 import Link from "next/link";
 
 export default async function Home() {
   const session = await auth();
   const user = session?.user;
+  const email = user?.email;
+  const res = email ? await fetchUserNameByEmail(email) : null;
+  const name = res?.data?.name;
   return (
     <div className="min-h-screen">
       {/* <span className="text-[21.3rem] font-black text-purple-700 tracking-wider">Kiton</span> */}
@@ -17,7 +20,7 @@ export default async function Home() {
         <div className="p-8 space-x-4">
 
           <span>
-            <span className="text-purple-500 text-lg font-bold tracking-wider">{user.name}, </span>
+            <span className="text-purple-500 text-lg font-bold tracking-wider">{name}, </span>
             You&apos;re authenticated!
           </span>
           <Button onClick={logoutAction} variant="destructive" className="cursor-pointer">Logout</Button>
